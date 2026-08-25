745f112f99fbe43cf7d141f63fc413bc4506c169
Sheran <it24100340@my.sliit.lk>
2026-08-25 13:16:24 +0530

    Prepare backend for Render deployment

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const errorHandler = require('./backend/middleware/errorHandler');
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;;
 
   // 1. CORS Configuration
   app.use(cors());
